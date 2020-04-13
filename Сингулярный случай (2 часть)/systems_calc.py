f0ecb483a2cd7bca5c9fa481c4af0a93c5eba876
Alexey Poghilenkov <alex_pozhilenkov@adoriasoft.com>
2020-04-13 21:27:43 +0300

    add new validation

--- Сингулярный случай (2 часть)/systems_calc.py
+++ Сингулярный случай (2 часть)/systems_calc.py
@@ -132,6 +132,8 @@ f_4_1 = Matrix([[e**(a*b)*(2*b*a**2*m + 2*a + 2*a*m), e**(a*b)*(2*b*a**2*m - 2*a
 [0, 2 + m, 0, (1 + m)*4*a]]
 )
 
+#print(d_1_1.det())
+
 ######
 # U_0[psi_1] matrix element 1
 expr1 = e**(a*b)*(2*b*a**2*m + 2*a + 2*a*m)
@@ -142,7 +144,69 @@ expr4 = e**(-a*b)*(2*b*a**2*m + 2*a)
 res = d_1_1.det() * expr1 + d_3_1.det() * expr2 + f_1_1.det() * expr3 + f_3_1.det() * expr4
 print(sp.expand(res))
 
-#print(d_1_1.det())
+# U_0[psi_1] matrix element 2
+expr1 = e**(a*b)*(2*b*a**2*m + 2*a + 2*a*m)
+expr2 = e**(a*b)*(2*b*a**2*m - 2*a)
+expr3 = e**(-a*b)*(-2*b*a**2*m + 2*a + 2*a*m)
+expr4 = e**(-a*b)*(2*b*a**2*m + 2*a)
+
+res = d_2_1.det() * expr1 + d_4_1.det() * expr2 + f_2_1.det() * expr3 + f_4_1.det() * expr4
+print(sp.expand(res))
+
+# U_0[psi_1] matrix element 3
+expr1 = e**(a*b)*(-2*b*a**2*m*g - 2*a*m*g + 2*a*l)
+expr2 = e**(a*b)*(-2*b*a**2*m*g + 4*a*g + 2*a*l)
+expr3 = e**(-a*b)*(-2*b*a**2*m*g + 2*a*m*g - 2*a*l)
+expr4 = e**(-a*b)*(2*b*a**2*m*g + 4*a*g + 2*a*l)
+
+res = d_1_1.det() * expr1 + d_3_1.det() * expr2 + f_1_1.det() * expr3 + f_3_1.det() * expr4
+print(sp.expand(res))
+
+# U_0[psi_1] matrix element 4
+expr1 = e**(a*b)*(-2*b*a**2*m*g - 2*a*m*g + 2*a*l)
+expr2 = e**(a*b)*(-2*b*a**2*m*g + 4*a*g + 2*a*l)
+expr3 = e**(-a*b)*(-2*b*a**2*m*g + 2*a*m*g - 2*a*l)
+expr4 = e**(-a*b)*(2*b*a**2*m*g + 4*a*g + 2*a*l)
+
+res = d_2_1.det() * expr1 + d_4_1.det() * expr2 + f_2_1.det() * expr3 + f_4_1.det() * expr4
+print(sp.expand(res))
+
+######
+# U_1[psi_0] matrix element 1
+expr1 = (2*a + 2*a*m)
+expr2 = (-2*a)
+expr3 = (2*a + 2*a*m)
+expr4 = (2*a)
+
+res = d_1_0.det() * expr1 + d_3_0.det() * expr2 + f_1_0.det() * expr3 + f_3_0.det() * expr4
+print(sp.expand(res))
+
+# U_1[psi_0] matrix element 2
+expr1 = (2*a + 2*a*m)
+expr2 = (-2*a)
+expr3 = (2*a + 2*a*m)
+expr4 = (2*a)
+
+res = d_2_0.det() * expr1 + d_4_0.det() * expr2 + f_2_0.det() * expr3 + f_4_0.det() * expr4
+print(sp.expand(res))
+
+# U_1[psi_0] matrix element 3
+expr1 = 0
+expr2 = (2 + m)
+expr3 = 0
+expr4 = (-2 - m)
+
+res = d_1_0.det() * expr1 + d_3_0.det() * expr2 + f_1_0.det() * expr3 + f_3_0.det() * expr4
+print(sp.expand(res))
+
+# U_1[psi_0] matrix element 4
+expr1 = 0
+expr2 = (2 + m)
+expr3 = 0
+expr4 = (-2 - m)
+
+res = d_2_0.det() * expr1 + d_4_0.det() * expr2 + f_2_0.det() * expr3 + f_4_0.det() * expr4
+print(sp.expand(res))
 
 
 
